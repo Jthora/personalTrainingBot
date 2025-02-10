7e0a7cf3eb68d83ec84037b701d63249602f5342
Jordan Trana [Jono Tho'ra] <jordan.trana@gmail.com>
2025-02-09 18:50:41 -0800

    Settings Upgrade
    drop downs applied

--- my-training-bot/src/components/CardSelector/CardSelector.tsx
+++ my-training-bot/src/components/CardSelector/CardSelector.tsx
@@ -5,6 +5,9 @@ import styles from './CardSelector.module.css';
 
 const CardSelector: React.FC = () => {
     const [trainingModules, setTrainingModules] = useState<TrainingModule[]>([]);
+    const [visibleModules, setVisibleModules] = useState<Set<string>>(new Set());
+    const [visibleSubModules, setVisibleSubModules] = useState<Set<string>>(new Set());
+    const [visibleCardDecks, setVisibleCardDecks] = useState<Set<string>>(new Set());
 
     useEffect(() => {
         const cache = TrainingModuleCache.getInstance();
@@ -12,6 +15,18 @@ const CardSelector: React.FC = () => {
             setTrainingModules(Array.from(cache.cache.values()));
         }
     }, []);
+
+    const toggleVisibility = (id: string, setVisible: React.Dispatch<React.SetStateAction<Set<string>>>) => {
+        setVisible(prev => {
+            const newSet = new Set(prev);
+            if (newSet.has(id)) {
+                newSet.delete(id);
+            } else {
+                newSet.add(id);
+            }
+            return newSet;
+        });
+    };
 
     const toggleModuleSelection = (id: string) => {
         const cache = TrainingModuleCache.getInstance();
@@ -39,6 +54,7 @@ const CardSelector: React.FC = () => {
 
     return (
         <div className={styles.cardSelector}>
+            <h3>Training Module Select</h3>
             {trainingModules.map(module => (
                 <div key={module.id} className={styles.module}>
                     <input
@@ -47,42 +63,57 @@ const CardSelector: React.FC = () => {
                         onChange={() => toggleModuleSelection(module.id)}
                     />
                     <span>{module.name}</span>
-                    <div className={styles.subModules}>
-                        {module.submodules.map(subModule => (
-                            <div key={subModule.id} className={styles.subModule}>
-                                <input
-                                    type="checkbox"
-                                    checked={TrainingModuleCache.getInstance().isSubModuleSelected(subModule.id)}
-                                    onChange={() => toggleSubModuleSelection(subModule.id)}
-                                />
-                                <span>{subModule.name}</span>
-                                <div className={styles.cardDecks}>
-                                    {subModule.cardDecks.map(deck => (
-                                        <div key={deck.id} className={styles.cardDeck}>
-                                            <input
-                                                type="checkbox"
-                                                checked={TrainingModuleCache.getInstance().isCardDeckSelected(deck.id)}
-                                                onChange={() => toggleCardDeckSelection(deck.id)}
-                                            />
-                                            <span>{deck.name}</span>
-                                            <div className={styles.cards}>
-                                                {deck.cards.map(card => (
-                                                    <div key={card.id} className={styles.card}>
-                                                        <input
-                                                            type="checkbox"
-                                                            checked={TrainingModuleCache.getInstance().isCardSelected(card.id)}
-                                                            onChange={() => toggleCardSelection(card.id)}
-                                                        />
-                                                        <span>{card.title}</span>
-                                                    </div>
-                                                ))}
-                                            </div>
+                    <button className={styles.dropdownButton} onClick={() => toggleVisibility(module.id, setVisibleModules)}>
+                        {visibleModules.has(module.id) ? '▬' : '▼'}
+                    </button>
+                    {visibleModules.has(module.id) && (
+                        <div className={styles.subModules}>
+                            {module.submodules.map(subModule => (
+                                <div key={subModule.id} className={styles.subModule}>
+                                    <input
+                                        type="checkbox"
+                                        checked={TrainingModuleCache.getInstance().isSubModuleSelected(subModule.id)}
+                                        onChange={() => toggleSubModuleSelection(subModule.id)}
+                                    />
+                                    <span>{subModule.name}</span>
+                                    <button className={styles.dropdownButton} onClick={() => toggleVisibility(subModule.id, setVisibleSubModules)}>
+                                        {visibleSubModules.has(subModule.id) ? '▬' : '▼'}
+                                    </button>
+                                    {visibleSubModules.has(subModule.id) && (
+                                        <div className={styles.cardDecks}>
+                                            {subModule.cardDecks.map(deck => (
+                                                <div key={deck.id} className={styles.cardDeck}>
+                                                    <input
+                                                        type="checkbox"
+                                                        checked={TrainingModuleCache.getInstance().isCardDeckSelected(deck.id)}
+                                                        onChange={() => toggleCardDeckSelection(deck.id)}
+                                                    />
+                                                    <span>{deck.name}</span>
+                                                    <button className={styles.dropdownButton} onClick={() => toggleVisibility(deck.id, setVisibleCardDecks)}>
+                                                        {visibleCardDecks.has(deck.id) ? '▬' : '▼'}
+                                                    </button>
+                                                    {visibleCardDecks.has(deck.id) && (
+                                                        <div className={styles.cards}>
+                                                            {deck.cards.map(card => (
+                                                                <div key={card.id} className={styles.card}>
+                                                                    <input
+                                                                        type="checkbox"
+                                                                        checked={TrainingModuleCache.getInstance().isCardSelected(card.id)}
+                                                                        onChange={() => toggleCardSelection(card.id)}
+                                                                    />
+                                                                    <span>{card.title}</span>
+                                                                </div>
+                                                            ))}
+                                                        </div>
+                                                    )}
+                                                </div>
+                                            ))}
                                         </div>
-                                    ))}
+                                    )}
                                 </div>
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
+                    )}
                 </div>
             ))}
         </div>

0a1ae4345bacbf49b40e978c435fa4fb95982a6c
Jordan Trana [Jono Tho'ra] <jordan.trana@gmail.com>
2025-02-17 04:05:06 -0800

    Added Jono Tho'ra Workout Category
    Now I can train to become who I need to be... for Aurora Tho'ra.

--- my-training-bot/src/utils/workoutCategoryPaths.ts
+++ my-training-bot/src/utils/workoutCategoryPaths.ts
@@ -5,6 +5,7 @@ export const workoutCategoryPaths: { [key: string]: () => Promise<any> } = {
     combat: () => import("../data/training_coach_data/workouts/combat.json"),
     coordination: () => import("../data/training_coach_data/workouts/coordination.json"),
     endurance: () => import("../data/training_coach_data/workouts/endurance.json"),
+    jono_thora: () => import("../data/training_coach_data/workouts/jono_thora.json"),
     mental: () => import("../data/training_coach_data/workouts/mental.json"),
     mobility: () => import("../data/training_coach_data/workouts/mobility.json"),
     strength: () => import("../data/training_coach_data/workouts/strength.json"),
